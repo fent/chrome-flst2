eff133271773510c43c7095deee1b1c4850ad225
fent <933490+fent@users.noreply.github.com>
2015-04-14 23:49:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+flst2
+=====
+
+A slightly faster version of FLST (Focus Last Selected Tab).
---
+++ background.js
@@ -0,0 +1,48 @@
+/* global chrome */
+
+var windows = {};
+
+chrome.tabs.query({ active: true }, function(tabs) {
+  tabs.forEach(function(tab) {
+    windows[tab.windowId] = [tab.id];
+  });
+});
+
+function removeTab(tabID, winID) {
+  var list = windows[winID];
+  var i = list.indexOf(tabID);
+  if (i > -1) { list.splice(i, 1); }
+}
+
+chrome.tabs.onActivated.addListener(function(info) {
+  if (!windows[info.windowId]) {
+    windows[info.windowId] = [info.tabId];
+  } else {
+    removeTab(info.tabId, info.windowId);
+    windows[info.windowId].push(info.tabId);
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabID, info) {
+  if (info.isWindowClosing) {
+    delete windows[info.windowId];
+    return;
+  }
+  var list = windows[info.windowId];
+  if (list[list.length - 1] === tabID) {
+    chrome.tabs.update(list[list.length - 2], { active: true });
+  }
+  removeTab(tabID, info.windowId);
+});
+
+chrome.tabs.onDetached.addListener(function(tabID, info) {
+  removeTab(tabID, info.oldWindowId);
+});
+
+chrome.tabs.onAttached.addListener(function(tabID, info) {
+  if (!windows[info.newWindowId]) {
+    windows[info.newWindowId] = [tabID];
+  } else {
+    windows[info.newWindowId].push(tabID);
+  }
+});
